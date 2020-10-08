c71be48a9d557b31068632ca673e04a6a79329ca
pietroDamico <p.damico73@gmail.com>
2020-10-08 23:55:25 +0200

    Modifiche player aggiunta audio, movimenti, fuoco e alvaro vitali

---
+++ Assets/Scripts/Alvaro.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alvaro : MonoBehaviour
+{
+
+    public Rigidbody2D target;
+    private Rigidbody2D body;
+    private Vector2 movement;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+
+        float angle =  Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        body.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+
+    }
+
+    void FixedUpdate()
+    {
+        move(movement);
+    }
+
+    private void move(Vector2 direction)
+    {
+        body.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float timeToLive = 2f;
+    private float spawnTime;
+
+    private void Start()
+    {
+        spawnTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - spawnTime > timeToLive)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Bullet detected");
+        Destroy(this.gameObject);
+    }
+    
+    
+
+
+}
